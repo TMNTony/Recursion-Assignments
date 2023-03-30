31fd6b53f79876d959f279bc1d9c6f40c4270fdc
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-30 10:23:12 -0500

    Add FibRec

--- src/index.js
+++ src/index.js
@@ -18,4 +18,13 @@ function fibIt(num) {
   }
   return array;
 }
-console.log(fibIt(8));
+
+function fibRec(num) {
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+  const previous = fibRec(num - 1);
+  const current = previous[previous.length - 1] + previous[previous.length - 2];
+  return [...previous, current];
+}
+
+console.log(fibRec(7));
