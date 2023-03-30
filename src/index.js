52f37c3ef35e0e747b2f3e5b02f83900a4187064
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-30 16:13:14 -0500

    Create mergeSort

--- src/index.js
+++ src/index.js
@@ -29,4 +29,37 @@ function fibRec(num) {
   return [...previous, current];
 }
 
-output.innerHTML = fibIt(5);
+function mergeSort(array) {
+  if (array.length < 2) return array;
+
+  const middle = Math.ceil(array.length / 2);
+  const first = array.slice(0, middle);
+  const second = array.slice(-middle);
+
+  mergeSort(first);
+  mergeSort(second);
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i <= first.length - 1 && j <= second.length - 1) {
+    if (first[i] < second[j]) {
+      array[k++] = first[i++];
+    } else {
+      array[k++] = second[j++];
+    }
+  }
+
+  for (; i <= first.length - 1; i++) {
+    array[k++] = first[i];
+  }
+
+  for (; j <= second.length - 1; j++) {
+    array[k++] = second[j];
+  }
+  return array;
+}
+
+output.innerHTML = mergeSort([5, 3, 2, 4, 6, 9, 11]);
+console.log(mergeSort([5, 3, 2, 4, 6, 9, 11]));
